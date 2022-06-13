35b17325ac8eee9429b58ae0901d82c63e7ab73b
Rajiv Shah <rshah@pobox.com>
2022-06-13 00:03:43 +0000

    Cleaned up code

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ import gradio as gr
 
 asr = pipeline("automatic-speech-recognition", "facebook/wav2vec2-base-960h")
 
+classifier = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion")
+
 def transcribe(audio):
     text = asr(audio)["text"]
     return text
-
-classifier = pipeline(
-    "text-classification",
-    model="bhadresh-savani/distilbert-base-uncased-emotion")
 
 def speech_to_text(speech):
     text = asr(speech)["text"]
@@ -25,17 +23,17 @@ def text_to_sentiment(text):
 demo = gr.Blocks()
 
 with demo:
-    #audio_file = gr.Audio(type="filepath")
-    audio_file = gr.inputs.Audio(source="microphone", type="filepath")
-    text = gr.Textbox()
-    label = gr.Label()
-    saved = gr.Textbox()
-    savedAll = gr.Textbox()
-    
-    b1 = gr.Button("Recognize Speech")
-    b2 = gr.Button("Classify Sentiment")
 
+    audio_file = gr.inputs.Audio(source="microphone", type="filepath")
+    b1 = gr.Button("Recognize Speech") 
     b1.click(speech_to_text, inputs=audio_file, outputs=text)
+    text = gr.Textbox()
+ 
+    b2 = gr.Button("Classify Sentiment")
     b2.click(text_to_sentiment, inputs=text, outputs=label)
+    label = gr.Label()
+    
+    
+    
     
 demo.launch(share=True)
